a4c2730201aff43335042e1a681efc5d8ad7125f
Jordan Wolter <jordan.wolter@icloud.com>
2023-02-07 16:22:23 -0600

    inital commit

---
+++ src/main/java/io/nhlspringboot/nhldashboard/NhlDashboardApplication.java
@@ -0,0 +1,13 @@
+package io.nhlspringboot.nhldashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhlDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhlDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/nhlspringboot/nhldashboard/NhlDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nhlspringboot.nhldashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhlDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
